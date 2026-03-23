ae178403b07d95f18ea6ebd1a507eedc63c78870
lashihollis <105299696+lashihollis@users.noreply.github.com>
2026-03-23 19:32:23 +0000

    removed tables added by ai the werent needed--patients reported data only includes vitals

--- analytics_engineering_project/models/intermediate/int_diagnosis/int_diagnosis__unioned.sql
+++ analytics_engineering_project/models/intermediate/int_diagnosis/int_diagnosis__unioned.sql
@@ -1,8 +1,7 @@
 with unioned as (
     select * from {{ ref('stg_payer__diagnosis') }}
     union all
-    select * from {{ ref('stg_ehr__diagnosis') }}    union all
-    select * from {{ ref('stg_patient__diagnosis') }}
+    select * from {{ ref('stg_ehr__diagnosis') }}
 )
 
 select *
--- analytics_engineering_project/models/intermediate/int_encounters/int_encounters__unioned.sql
+++ analytics_engineering_project/models/intermediate/int_encounters/int_encounters__unioned.sql
@@ -2,8 +2,6 @@ with unioned as (
     select * from {{ ref('stg_payer__encounters') }}
     union all
     select * from {{ ref('stg_ehr__encounters') }}
-    union all
-    select * from {{ ref('stg_patient_reported__encounters') }}
 )
 
 select *
